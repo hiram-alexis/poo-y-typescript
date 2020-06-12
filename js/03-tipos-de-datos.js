7c07897998534b2fa828fb5496f8b1551b4078b5
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-06-12 03:46:06 -0500

    150. Variables y tipos de datos en TS
    Como declarar los tipos de datos en TS. #4

---
+++ js/03-tipos-de-datos.js
@@ -0,0 +1,13 @@
+// String
+var cadena = "alexospears.com";
+// Number
+var numero = 12;
+// Boleano
+var verdadero_falso = true;
+// Any
+var cualquiera = "hola";
+cualquiera = 77;
+// Arrays
+var lenguajes = ["PHP", "JS", "CSS"];
+var years = ["Doce", 12, 13, 14];
+console.log(cadena, numero, verdadero_falso, cualquiera, lenguajes, years);
---
+++ js/03-tipos-de-datos.ts
@@ -0,0 +1,22 @@
+// String
+
+let cadena: string = "alexospears.com";
+
+// Number
+
+let numero : number = 12;
+
+// Boleano
+let verdadero_falso: boolean = true;
+
+// Any
+
+let cualquiera: any = "hola";
+cualquiera = 77;
+
+// Arrays
+var lenguajes: Array<string> = ["PHP", "JS", "CSS"];
+
+let years: any[] = ["Doce",12,13,14];
+
+console.log(cadena, numero, verdadero_falso, cualquiera, lenguajes, years);
