5846c5850ec7392d5dbaa488c0b3e588e6e46a15
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-06-13 02:19:30 -0500

    153. Let vs Var en TS
    La diferencia de una variable local de una global. #4

--- js/03-tipos-de-datos.js
+++ js/03-tipos-de-datos.js
@@ -9,6 +9,15 @@ var verdadero_falso = true;
 var cualquiera = "hola";
 cualquiera = 77;
 // Arrays
-var lenguajes = ["PHP", "JS", "CSS"];
-var years = ["Doce", 12, 13, 14];
+var lenguajes = ["PHP", "JS", "CSS"]; // Definir la variable como "var" la vuelve global
+var years = ["Doce", 12, 13, 14]; // Definir la variable como "let" la vuelve local
+// Let vs var
+var numero1 = 10;
+var numero2 = 12;
+if (numero1 == 10) {
+    var numero1_1 = 44;
+    var numero2_1 = 55;
+    console.log("Variables locales = ", numero1_1, " , ", numero2_1);
+}
+console.log("Variables globales = ", numero1, " , ", numero2);
 console.log(cadena, numero, verdadero_falso, cualquiera, lenguajes, years);
--- js/03-tipos-de-datos.ts
+++ js/03-tipos-de-datos.ts
@@ -19,8 +19,18 @@ let cualquiera: any = "hola";
 cualquiera = 77;
 
 // Arrays
-var lenguajes: Array<string> = ["PHP", "JS", "CSS"];
+var lenguajes: Array<string> = ["PHP", "JS", "CSS"]; // Definir la variable como "var" la vuelve global
 
-let years: any[] = ["Doce",12,13,14];
+let years: any[] = ["Doce",12,13,14]; // Definir la variable como "let" la vuelve local
 
+// Let vs var
+var numero1=10;
+var numero2=12;
+
+if(numero1 ==10){
+	let numero1 =44;
+	let numero2 =55;
+	console.log("Variables locales = "numero1," , ",numero2);
+}
+console.log("Variables globales = "numero1," , ",numero2);
 console.log(cadena, numero, verdadero_falso, cualquiera, lenguajes, years);
